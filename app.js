b5a77a4b6df7b3d7f5d0d9b4e37cc9df1b4984e3
ganesh-goyal <gauravgoyal7075@gmail.com>
2026-04-14 16:35:12 +0530

    all file  added on git

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const fs = require("fs");
+const { exec } = require("child_process");
+
+const app = express();
+app.use(express.json());
+
+// 🚨 Hardcoded secret
+const SECRET_KEY = "my-super-secret-key";
+
+// In-memory board
+let board = Array(9).fill("");
+
+// Home
+app.get("/", (req, res) => {
+  res.send("TicTacToe JS DevSecOps App Running");
+});
+
+// Move (no validation)
+app.post("/move", (req, res) => {
+  const { position, player } = req.body;
+
+  if (board[position] === "") {
+    board[position] = player;
+    res.json(board);
+  } else {
+    res.status(400).send("Invalid move");
+  }
+});
+
+// 🚨 Command Injection
+app.get("/run", (req, res) => {
+  const cmd = req.query.cmd;
+  exec(cmd, (err, stdout) => {
+    res.send(stdout);
+  });
+});
+
+// 🚨 File Read (Path Traversal)
+app.get("/read", (req, res) => {
+  const file = req.query.file;
+  const data = fs.readFileSync(file, "utf8");
+  res.send(data);
+});
+
+// 🚨 Debug info leak
+app.get("/debug", (req, res) => {
+  res.json(process.env);
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
